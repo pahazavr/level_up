aefff5c9256419549e7476361bdc9d22f020b236
Pavel Palmin <pavel.palmin@t-systems.com>
2021-10-31 15:04:14 +0300

    Lesson 2: Homework
    * added solution for task 4

---
+++ lesson_2/homework/Task_2_4.java
@@ -0,0 +1,126 @@
+package homework;
+
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.Stack;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+    Написать функцию next() для получения следующего элемента после текущего в бинарном дереве поиска.
+ */
+public class Task_2_4 {
+
+    private int next(BinaryTree tree, int data) {
+        int diff = tree.getRoot().getData();
+        int result = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(tree.getRoot());
+
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            if(node.data > data) {
+                if (node.data - data < diff) {
+                    result = node.data;
+                    diff = node.data - data;
+                }
+            }
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+
+        if(result != 0) return result;
+
+        throw new IllegalArgumentException();
+    }
+
+    @Data
+    static class BinaryTree {
+        TreeNode root;
+
+        BinaryTree(int[] data) {
+            for (int d : data) {
+                insert(d);
+            }
+        }
+
+        void insert(int data) {
+            if (root == null) {
+                root = new TreeNode(data);
+            } else {
+                insert(root, data);
+            }
+        }
+
+        void insert(TreeNode node, int data) {
+            if (node.data <= data) {
+                if (node.right != null) {
+                    insert(node.right, data);
+                } else {
+                    node.right = new TreeNode(data);
+                }
+            } else {
+                if (node.left != null) {
+                    insert(node.left, data);
+                } else {
+                    node.left = new TreeNode(data);
+                }
+            }
+        }
+    }
+
+    @Data
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    @Test
+    public void testCases() {
+        BinaryTree tree = new BinaryTree(new int[]{20, 7, 4, 6, 9, 35, 31, 40, 28, 38, 52});
+        int expected = 6;
+        assertEquals(expected, next(tree, 4));
+
+        expected = 7;
+        assertEquals(expected, next(tree, 6));
+
+        expected = 9;
+        assertEquals(expected, next(tree, 7));
+
+        expected = 28;
+        assertEquals(expected, next(tree, 20));
+
+        expected = 31;
+        assertEquals(expected, next(tree, 28));
+
+        expected = 35;
+        assertEquals(expected, next(tree, 31));
+
+        expected = 38;
+        assertEquals(expected, next(tree, 35));
+
+        expected = 40;
+        assertEquals(expected, next(tree, 38));
+
+        expected = 52;
+        assertEquals(expected, next(tree, 40));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenLastElementThenThrowException() {
+        BinaryTree tree = new BinaryTree(new int[]{20, 7, 4, 6, 9, 35, 31, 40, 28, 38, 52});
+        next(tree, 52);
+    }
+}
